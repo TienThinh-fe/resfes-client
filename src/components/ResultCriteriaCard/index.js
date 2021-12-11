718319ae33723a266fa7cf8e61d2bb3aefc133bf
TienThinh-fe <thinhhtde150233@fpt.edu.vn>
2021-12-11 23:32:05 +0700

    fix: format score

--- src/components/ResultCriteriaCard/index.js
+++ src/components/ResultCriteriaCard/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import ScoreSlider from 'components/ScoreSlider'
 
 
-const fontStyles = { fontStyle: 'normal', fontSize: 16, color: 'black'}
+const fontStyles = { fontStyle: 'normal', fontSize: 16, color: 'black' }
 
 function ResultCriteriaCard(props) {
     const { weights, score, color, title } = props
@@ -34,9 +34,29 @@ function ResultCriteriaCard(props) {
                 flex: .1,
                 justifyContent: 'flex-end',
                 ...fontStyles
-            }}>{score}</p>
+            }}>{round10(score, -1)}</p>
         </div>)
 }
+
+function decimalAdjust(type, value, exp) {
+    if (typeof exp === 'undefined' || +exp === 0) {
+        return Math[type](value)
+    }
+    value = +value
+    exp = +exp
+
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+        return NaN
+    }
+    
+    value = value.toString().split('e')
+    value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)))
+   
+    value = value.toString().split('e')
+    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp))
+}
+
+const round10 = (value, exp) => decimalAdjust('round', value, exp)
 
 ResultCriteriaCard.propTypes = {
     weights: PropTypes.string.isRequired,
